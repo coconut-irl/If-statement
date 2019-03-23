1cfc7950718ccc47163187a8d17f7cc23b059a4d
coconut-irl <coconutx206@gmail.com>
2019-03-23 18:01:44 +0530

    Initial commit

---
+++ even.py
@@ -0,0 +1,9 @@
+# Asks for user input and prints if given number is even or odd
+
+number = float(input("Tell me a number: "))
+if number % 2 == 0:
+    print(int(number), "is even.")
+elif number % 2 == 1:
+    print(int(number), "is odd.")
+else:
+    print(number, "is very strange.")
